415c7e0ce4555d2fd31dca264aafac3e2a9c3b66
Wayne Haworth <waynehaworth@gmail.com>
2025-11-13 13:37:54 +0000

    adding react frontend

--- backend/index.js
+++ backend/index.js
@@ -5,7 +5,9 @@ import cors from 'cors'
 const app = express();
 app.use(cors())
 
+
 app.use(express.json())
+app.use(express.static('dist'))
 
 app.use(morgan(function (tokens, req, res) {
   return [
