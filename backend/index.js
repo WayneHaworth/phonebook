2b5bcd3f7402a8d72058337e7ca8d1fd5696f31f
Wayne Haworth <waynehaworth@gmail.com>
2025-11-13 13:42:17 +0000

    uibuild

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,7 @@
 import express from "express"
 import morgan from 'morgan'
-import cors from 'cors'
 
 const app = express();
-app.use(cors())
-
 
 app.use(express.json())
 app.use(express.static('dist'))
